f0094fb6f87df654b66e6da621c02dcfff9e5b70
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-09 18:22:35 +0100

    Completed

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,7 @@ const winOrLooseTextElement = document.getElementById('win-or-loose');
 const playAgainElement = document.getElementById('play-again');
 const showRulesElement = document.getElementById('show-rules');
 const hideRulesElement = document.getElementById('hide-rules');
-const rulesElement = document.getElementById('modal-simple')
+const rulesElement = document.getElementById('modal-simple');
 const houseChoiceElement = {
     children: [
         {
@@ -27,52 +27,42 @@ const houseChoiceElement = {
         }
     ]
 }
-const gameRules = {
-    children: [
-        {
-            children: [],
-            value: 'win',
-            winners: ['paper', 'rock', 'scissors'],
-            loosers: ['rock', 'scissors', 'paper'],
-        },
-        {
-            value: 'tie',
-            winners: ['paper', 'rock', 'scissors'],
-            loosers: ['paper', 'rock', 'scissors'],
-        },
-        {
-            value: 'loose',
-            winners: ['paper', 'rock', 'scissors'],
-            loosers: ['scissors', 'paper', 'rock'],
-        }
-    ]
-}
 
 
-const openRules = () => {
-    rulesElement.classList.replace('hide', 'show');
-}
-showRulesElement.addEventListener('click', openRules)
-
-const closeRules = () => {
-    rulesElement.classList.replace('show', 'hide');
-}
-hideRulesElement.addEventListener('click', closeRules)
+let ownScore = 0;
+let houseScore = 0;
 
 const addChoiceClasses = event => {
     const classButton = event.target.classList[0];
     const classButtonChoice = event.target.classList[1];
-    const randomChoice = Math.floor(Math.random() * houseChoiceElement.children.length)
-    const houseChoice = houseChoiceElement.children[randomChoice].classList[0]
-    const houseChoice2 = houseChoiceElement.children[randomChoice].classList[1]
-    let ownScore = 0
-    let houseScore = 0
-    allButtonsElement.classList.add('hidediv')
+    const randomChoice = Math.floor(Math.random() * houseChoiceElement.children.length);
+    const houseChoice = houseChoiceElement.children[randomChoice].classList[0];
+    const houseChoice2 = houseChoiceElement.children[randomChoice].classList[1];
+    allButtonsElement.classList.add('hidediv');
     ownSelectionElement.classList.remove('empty-result');
-    ownSelectionElement.classList.add(classButtonChoice, classButton)
+    ownSelectionElement.classList.add(classButtonChoice, classButton);
     houseSelectionElement.classList.remove('empty-result');
-    houseSelectionElement.classList.add(houseChoice, houseChoice2)
-    resultDivElement.classList.remove('hidediv')
+    houseSelectionElement.classList.add(houseChoice, houseChoice2);
+    resultDivElement.classList.remove('hidediv');
+    const ownElection = ownSelectionElement.classList[0];
+    const houseElection = houseSelectionElement.classList[1];
+    if (ownElection === 'paper' && houseElection === 'rock' ||
+        ownElection === 'rock' && houseElection === 'scissors' ||
+        ownElection === 'scissors' && houseElection === 'paper') {
+        winOrLooseTextElement.textContent = 'YOU WIN';
+        ownScore += 1;
+    } else if (ownElection === 'paper' && houseElection === 'paper' ||
+        ownElection === 'rock' && houseElection === 'rock' ||
+        ownElection === 'scissors' && houseElection === 'scissors') {
+        winOrLooseTextElement.textContent = 'TIE'
+    } else if (ownElection === 'paper' && houseElection === 'scissors' ||
+        ownElection === 'rock' && houseElection === 'paper' ||
+        ownElection === 'scissors' && houseElection === 'rock') {
+        winOrLooseTextElement.textContent = 'YOU LOSE';
+        houseScore += 1;
+    }
+    ownScoreElement.textContent = ownScore;
+    houseScoreElement.textContent = houseScore;
 }
 allButtonsElement.addEventListener('click', addChoiceClasses)
 
@@ -85,3 +75,13 @@ const restart = event => {
     resultDivElement.classList.add('hidediv')
 }
 playAgainElement.addEventListener('click', restart)
+
+const openRules = () => {
+    rulesElement.classList.replace('hide', 'show');
+}
+showRulesElement.addEventListener('click', openRules)
+
+const closeRules = () => {
+    rulesElement.classList.replace('show', 'hide');
+}
+hideRulesElement.addEventListener('click', closeRules)
