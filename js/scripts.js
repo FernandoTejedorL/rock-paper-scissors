5260b4b28c6d1b91ddab71ae721b714791b98a03
FernandoTejedorL <fernandotejedorlopez@gmail.com>
2024-11-09 17:28:54 +0100

    pending rules

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ const allButtonsElement = document.getElementById('buttons');
 const buttonPaperElement = document.getElementById('button-paper');
 const buttonScissorsElement = document.getElementById('button-scissors');
 const buttonRockElement = document.getElementById('button-rock');
+const resultDivElement = document.getElementById('resultdiv')
 const ownSelectionElement = document.getElementById('own-selection');
 const houseSelectionElement = document.getElementById('house-selection');
 const winOrLooseTextElement = document.getElementById('win-or-loose');
@@ -12,6 +13,41 @@ const playAgainElement = document.getElementById('play-again');
 const showRulesElement = document.getElementById('show-rules');
 const hideRulesElement = document.getElementById('hide-rules');
 const rulesElement = document.getElementById('modal-simple')
+const houseChoiceElement = {
+    children: [
+        {
+            children: [],
+            classList: ['button', 'paper'],
+        },
+        {
+            classList: ['button', 'scissors'],
+        },
+        {
+            classList: ['button', 'rock']
+        }
+    ]
+}
+const gameRules = {
+    children: [
+        {
+            children: [],
+            value: 'win',
+            winners: ['paper', 'rock', 'scissors'],
+            loosers: ['rock', 'scissors', 'paper'],
+        },
+        {
+            value: 'tie',
+            winners: ['paper', 'rock', 'scissors'],
+            loosers: ['paper', 'rock', 'scissors'],
+        },
+        {
+            value: 'loose',
+            winners: ['paper', 'rock', 'scissors'],
+            loosers: ['scissors', 'paper', 'rock'],
+        }
+    ]
+}
+
 
 const openRules = () => {
     rulesElement.classList.replace('hide', 'show');
@@ -23,7 +59,29 @@ const closeRules = () => {
 }
 hideRulesElement.addEventListener('click', closeRules)
 
-const checkbutton = event => {
+const addChoiceClasses = event => {
+    const classButton = event.target.classList[0];
+    const classButtonChoice = event.target.classList[1];
+    const randomChoice = Math.floor(Math.random() * houseChoiceElement.children.length)
+    const houseChoice = houseChoiceElement.children[randomChoice].classList[0]
+    const houseChoice2 = houseChoiceElement.children[randomChoice].classList[1]
+    let ownScore = 0
+    let houseScore = 0
+    allButtonsElement.classList.add('hidediv')
+    ownSelectionElement.classList.remove('empty-result');
+    ownSelectionElement.classList.add(classButtonChoice, classButton)
+    houseSelectionElement.classList.remove('empty-result');
+    houseSelectionElement.classList.add(houseChoice, houseChoice2)
+    resultDivElement.classList.remove('hidediv')
 }
+allButtonsElement.addEventListener('click', addChoiceClasses)
 
-allButtonsElement.addEventListener('click', checkbutton)
+const restart = event => {
+    ownSelectionElement.classList.remove(ownSelectionElement.classList[0], ownSelectionElement.classList[1]);
+    ownSelectionElement.classList.add('empty-result')
+    houseSelectionElement.classList.remove(houseSelectionElement.classList[0], houseSelectionElement.classList[1]);
+    houseSelectionElement.classList.add('empty-result')
+    allButtonsElement.classList.remove('hidediv')
+    resultDivElement.classList.add('hidediv')
+}
+playAgainElement.addEventListener('click', restart)
